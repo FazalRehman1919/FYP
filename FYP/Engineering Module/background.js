7d301e5458ca855ab4c42b00742303fbaf474a54
Fazal Rehman <wekibangash1919@gmail.com>
2022-10-14 19:42:33 +0500

    I am Pushing FYP

---
+++ FYP/Engineering Module/background.js
@@ -0,0 +1,10 @@
+
+// amir project 
+chrome.extension.onRequest.addListener(function(prediction){
+    if (prediction == 1){
+        alert("Warning: Phishing detected!!");
+    }
+    else if (prediction == -1){
+        alert("No phishing detected");
+    }
+});
